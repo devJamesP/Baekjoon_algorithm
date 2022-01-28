740f3bd231f2b170b9dd85f0655f10a3255d446d
James <znzldh1234@gmail.com>
2022-01-28 20:55:33 +0900

    1181 2920 problem solution

---
+++ AlgorithmProblem/1181_Word_Sort.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+
+namespace AlgorithmProblem
+{
+    class _1181_Word_Sort
+    {
+        static void Problem_1181() { 
+        {
+            StreamReader sr = new StreamReader(Console.OpenStandardInput());
+            StreamWriter sw = new StreamWriter(Console.OpenStandardOutput());
+
+            int nWordCount = int.Parse(sr.ReadLine());
+            string[] strWordArr = new string[nWordCount];
+
+            // input
+            for (int i = 0; i < nWordCount; ++i)
+            {
+                strWordArr[i] = sr.ReadLine();
+            }
+
+            Array.Sort(strWordArr, new SortWord());
+            sw.WriteLine(strWordArr[0]);
+            for(int i = 1; i < strWordArr.Length; ++i)
+            {
+                if (strWordArr[i-1] != strWordArr[i])
+                {
+                    sw.WriteLine(strWordArr[i]);
+                }
+            }
+
+            sw.Flush();
+            sr.Close();
+            sw.Close();
+        }
+    }
+
+    class SortWord : IComparer
+    {
+        public int Compare(object x, object y)
+        {
+            string word1 = (string)x;
+            string word2 = (string)y;
+
+            if (word1.Length == word2.Length)
+            {
+                // 사전 비교
+                return word1.CompareTo(word2);
+            }
+            else
+            {
+                // 길이 비교
+                if (word1.Length > word2.Length)
+                {
+                    return 1;
+                }
+                else if (word1.Length <= word2.Length)
+                {
+                    return -1;
+                }
+            }
+            return -1;
+        }
+    }
+}
---
+++ AlgorithmProblem/2920_Scale.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+
+namespace AlgorithmProblem
+{
+    class _2920_Scale
+    {
+        static void Problem_2920()
+        {
+            StreamReader sr = new StreamReader(Console.OpenStandardInput());
+            StreamWriter sw = new StreamWriter(Console.OpenStandardOutput());
+
+            string strAscending = "12345678";
+            string strDescending = "87654321";
+
+            string strOutput = "mixed";
+            string[] strInputScaleArr = sr.ReadLine().Split(' ');
+
+            if (strInputScaleArr[0] == "1")
+            {
+                strOutput = "ascending";
+                for (int i = 1; i < strInputScaleArr.Length - 1; ++i)
+                {
+                    // Ascending Check
+                    if (strInputScaleArr[i] != (i + 1).ToString())
+                    {
+                        // mixed 
+                        strOutput = "mixed";
+                        break;
+                    }
+                }
+            } else if (strInputScaleArr[0] == "8")
+            {
+                strOutput = "descending";
+                for (int i = 1; i < strInputScaleArr.Length - 1; ++i)
+                {
+                    // Descending Check
+                    if (strInputScaleArr[i] != (strInputScaleArr.Length-i).ToString())
+                    {
+                        // mixed 
+                        strOutput = "mixed";
+                        break;
+                    }
+                }
+            }
+
+            sw.WriteLine(strOutput);
+
+            sw.Flush();
+            sr.Close();
+            sw.Close();
+        }
+    }
+}
