2950707465b9a328ccf444ed548e0b7e15770bb4
James <znzldh1234@gmail.com>
2022-01-29 14:01:01 +0900

    1259 problem clear

---
+++ AlgorithmProblem/1259_Pelindrome.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+
+namespace AlgorithmProblem
+{
+    class _1259_Pelindrome
+    {
+        static void Problem_1259() 
+        {
+            StreamReader sr = new StreamReader(Console.OpenStandardInput());
+            StreamWriter sw = new StreamWriter(Console.OpenStandardOutput());
+
+            string strPelindrome = "";
+
+            while (true)
+            {
+                strPelindrome = sr.ReadLine();
+                if (strPelindrome == "0")
+                {
+                    break;
+                }
+
+                int nMedianLength = (int)Math.Round((double)strPelindrome.Length / 2, 1);
+                int i = 0;
+                for(i = 0; i < nMedianLength; ++i)
+                {
+                    if (strPelindrome[i] != strPelindrome[strPelindrome.Length-1-i])
+                    {
+                        sw.WriteLine("no");
+                        break;
+                    }
+                }
+                if (i == nMedianLength)
+                {
+                    sw.WriteLine("yes");
+                }
+            }
+
+            sw.Flush();
+            sr.Close();
+            sw.Close();
+        }
+    }
+}
