68f3bdea0f2e04ba4ffabeee1f5d83c8f064bd98
James <znzldh1234@gmail.com>
2022-01-29 14:39:32 +0900

    2475 solution

---
+++ AlgorithmProblem/2475_Binomial Coefficient.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+namespace AlgorithmProblem
+{
+    class _2475_Binomial_Coefficient
+    {
+        static void Problem_2475()
+        {
+            StreamReader sr = new StreamReader(Console.OpenStandardInput());
+            StreamWriter sw = new StreamWriter(Console.OpenStandardOutput());
+
+            string[] strInputArr = sr.ReadLine().Split(' ');
+
+            int n = int.Parse(strInputArr[0]);
+            int k = int.Parse(strInputArr[1]);
+
+            int nResult = factorial(n, 1) / (factorial(k, 1) * factorial(n - k, 1));
+            sw.WriteLine(nResult);
+
+            sw.Flush();
+            sr.Close();
+            sw.Close();
+        }
+
+        static int factorial(int n, int sum)
+        {
+            if (n <= 1)
+            {
+                return sum;
+            }
+            return factorial(n-1, sum * n); 
+        }
+    }
+}
