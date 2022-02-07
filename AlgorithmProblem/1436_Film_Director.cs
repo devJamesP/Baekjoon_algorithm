b1bb9ec8c54641ddfbfa2aba0b3c760d9eea8c44
James <znzldh1234@gmail.com>
2022-02-07 09:17:03 +0900

    modify mainApi

--- AlgorithmProblem/1436_Film_Director.cs
+++ AlgorithmProblem/1436_Film_Director.cs
@@ -1,9 +1,23 @@
-﻿        static void Main(string[] args)
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace AlgorithmProblem
+{
+    class _1436_Film_Director
+    {
+        static void Main(string[] args)
         {
             StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
             StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+
+
+
+
 
             sw.Flush();
             sr.Close();
             sw.Close();
         }
+    }
+}
