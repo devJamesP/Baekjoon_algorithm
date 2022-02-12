f552d42add1d70a3c9850143c61a9e36c2d2f962
James <znzldh1234@gmail.com>
2022-02-12 18:20:37 +0900

    1874 solution

--- AlgorithmProblem/1874_Stack_Sequence.cs
+++ AlgorithmProblem/1874_Stack_Sequence.cs
@@ -1,16 +1,47 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Text;
 using System.IO;
 namespace AlgorithmProblem
 {
     class _1874_Stack_Sequence
     {
-        static void Main(string[] args)
+        static void Problem_1874()
         {
             StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
             StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+            StringBuilder sb = new StringBuilder();
 
+            Stack<int> stack = new Stack<int>();
+            int n = int.Parse(sr.ReadLine());
 
+            int num = 1;
+            int input;
+
+            for(int i = 0; i < n; ++i)
+            {
+                input = int.Parse(sr.ReadLine());
+                while (num <= input)
+                {
+                    stack.Push(num);
+                    sb.Append("+\n");
+                    ++num;
+                }
+
+                if (stack.Count > 0 && stack.Peek() == input)
+                {
+                    stack.Pop();
+                    sb.Append("-\n");
+                }
+                else
+                {
+                    sb.Clear();
+                    sb.Append("NO");
+                    break;
+                }
+            }
+
+            sw.WriteLine(sb.ToString());
             sw.Flush();
             sr.Close();
             sw.Close();
