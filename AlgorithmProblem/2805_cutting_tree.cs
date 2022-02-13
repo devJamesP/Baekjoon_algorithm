d5b5acc80927f3bea1e63428d59ca159ae69caf9
James <znzldh1234@gmail.com>
2022-02-13 13:11:59 +0900

    binarysearch_upper_lower

---
+++ AlgorithmProblem/2805_cutting_tree.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace AlgorithmProblem
+{
+    /*
+     * N : 나무의 수 (1 <= N <= 1_000_000)
+     * M : 상근이 놈이 집에 몰래 가져가려는 나무의 길이 (1 <= M <= 2_000_000_000)
+     * nTreeHeightArr : 나무 높이 배열
+     * nMaxCutHeight : 톱으로 자를 최대 길이(나무 낭비하면 안된다는데... 자르면 죽는거 아닌가)
+     * 
+     * 나무 높이를 설정하여 나무를 잘랐을 때 최소한 낭비 없이 자를 수 있는 최대 높이 구하기
+     */
+    class _2805_cutting_tree
+    {
+
+        static void Test(string[] args)
+        {
+            StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+            StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+
+            string[] strInput = sr.ReadLine().Split(' '); ;
+            int N = int.Parse(strInput[0]);
+            int M = int.Parse(strInput[1]);
+            int[] nTreeHeightArr = Array.ConvertAll(sr.ReadLine().Split(' '), int.Parse);
+            int nMaxCutHeight;
+
+            Array.Sort(nTreeHeightArr);
+            cuttingMaxTreeHeight(nTreeHeightArr, M, out nMaxCutHeight);
+
+            sw.WriteLine(nMaxCutHeight);
+            sw.Flush();
+            sr.Close();
+            sw.Close();
+        }
+
+        static void cuttingMaxTreeHeight(int[] nTreeHeightArr, int M, out int nMaxCutHeight)
+        {
+            int high = nTreeHeightArr[nTreeHeightArr.Length - 1];
+            int low = nTreeHeightArr[0];
+            int mid = 0;
+
+            int sumWoodHeight = 0;
+            while(low <= high)
+            {
+                mid = low + (high - low) / 2;
+                sumWoodHeight = cutTreeHeight(nTreeHeightArr, mid);
+                if (sumWoodHeight >= M)
+                {
+                    low = mid + 1;
+                }
+                else if (sumWoodHeight < M)
+                {
+                    high = mid - 1;
+                }
+            }
+            nMaxCutHeight = mid;
+        }
+
+        static int cutTreeHeight(int[] nTreeHeightArr, int cut)
+        {
+            int sum = 0;
+            for (int i = 0; i < nTreeHeightArr.Length; ++i)
+            {
+                if (nTreeHeightArr[i] > cut)
+                {
+                    sum += (nTreeHeightArr[i] - cut);
+                }
+            }
+            return sum;
+        }
+    }
+}
---
+++ AlgorithmProblem/Binary_Search.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmProblem
+{
+    class Binary_Search
+    {
+        static void binary_Search()
+        {
+            StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+            StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+
+            int[] nArr = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            int[] nSameArr = { 0, 1, 2, 3, 4, 4, 4, 5, 5, 6, 7 };
+
+            sw.WriteLine(binarySearch(nArr, -5));
+            sw.WriteLine(binarySearch(nArr, 2));
+            sw.WriteLine(binarySearch(nArr, 8));
+            sw.WriteLine(binarySearch(nArr, 11));
+            sw.WriteLine();
+
+            // upper bound 특성 상 찾고자 하는 값이 없고 큰 값일 경우 마지막 인덱스를 찾는 경우와 동일한 결과가 나옴.
+            sw.WriteLine(upperBound(nArr, -5));
+            sw.WriteLine(upperBound(nArr, 2));
+            sw.WriteLine(upperBound(nArr, 9));
+            sw.WriteLine(upperBound(nArr, 11));
+            sw.WriteLine();
+            // lower bound는 찾고자 하는 값이 없고 작은 값일 경우 첫번째 인덱스를 찾는 경우와 동일한 결과가 나옴.
+            sw.WriteLine(lowerBound(nArr, -5));
+            sw.WriteLine(lowerBound(nArr, 2));
+            sw.WriteLine(lowerBound(nArr, 9));
+            sw.WriteLine(lowerBound(nArr, 11));
+            sw.WriteLine();
+
+            sw.WriteLine(lowerBound(nSameArr, 4));
+            sw.WriteLine(upperBound(nSameArr, 4));
+            sw.WriteLine();
+
+            sw.Flush();
+            sr.Close();
+            sw.Close();
+        }
+
+        static int binarySearch(int[] arr, int target)
+        {
+            int low = 0;
+            int high = arr.Length;
+            int mid;
+
+            while (low < high)
+            {
+                mid = low + (high - low) / 2;
+                if (arr[mid] > target)
+                {
+                    high = mid - 1;
+                }
+                else if (arr[mid] < target)
+                {
+                    low = mid + 1;
+                }
+                else
+                {
+                    return mid;
+                }
+            }
+
+            return -1;
+        }
+
+        // 상한값 찾기(바로 위 초과)
+        static int upperBound(int[] arr, int target)
+        {
+            int low = 0;
+            int high = arr.Length;
+            int mid;
+
+            while (low < high)
+            {
+                mid = low + (high - low) / 2;
+                if (target < arr[mid])
+                {
+                    high = mid;
+                }
+                else
+                {
+                    low = mid + 1;
+                }
+            }
+
+            return low;
+        }
+
+        // 하한값 찾기(해당 값을 포함하는 이상 값)
+        static int lowerBound(int[] arr, int target)
+        {
+            int low = 0;
+            int high = arr.Length;
+            int mid;
+
+            while(low < high)
+            {
+                mid = low + (high - low) / 2;
+                if (target <= arr[mid])
+                {
+                    high = mid;
+                }
+                else
+                {
+                    low = mid + 1;
+                }
+            }
+
+            return low;
+        }
+    }
+}
