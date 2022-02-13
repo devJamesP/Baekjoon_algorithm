e2d432876d19a940d0b77881912aaa086e009e31
James <znzldh1234@gmail.com>
2022-02-13 19:17:16 +0900

    15829 solution

--- AlgorithmProblem/15829_Hashing.cs
+++ AlgorithmProblem/15829_Hashing.cs
@@ -7,31 +7,28 @@ namespace AlgorithmProblem
     {
         static void Problem_15829()
         {
+            const int multi = 31;
+            const int mod = 1234567891;
+
             StreamReader sr = new StreamReader(Console.OpenStandardInput());
             StreamWriter sw = new StreamWriter(Console.OpenStandardOutput());
 
             int nLength = int.Parse(sr.ReadLine());
             string strInput = sr.ReadLine();
-            ulong M = 1234567891;
 
-            ulong nResult = outputHashOfAlphabet(nLength, strInput) % M;
-            sw.WriteLine(nResult);
+            long sum = 0;
+            long r = 1;
+
+            for (int i = 0; i < nLength; ++i)
+            {
+                sum = (sum + (strInput[i] - 'a' + 1) * r) % mod;
+                r = (r * multi) % mod;
+            }
+            sw.WriteLine(sum % mod);
 
             sw.Flush();
             sr.Close();
             sw.Close();
-        }
-
-        static ulong outputHashOfAlphabet(int nLength, string strInput)
-        {
-            ulong sum = 0;
-            int r = 31;
-
-            for (int i = 0; i < nLength; ++i)
-            {
-                sum += (ulong)(strInput[i] - 'a' + 1) * (ulong)Math.Pow(r, i);
-            }
-            return sum;
         }
     }
 }
