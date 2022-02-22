d4c0909ebaf0e427accffab2edff110e770fb872
James <znzldh1234@gmail.com>
2022-02-22 10:44:46 +0900

    1676 solution

---
+++ AlgorithmProblem/1676_factorial_0_count.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace AlgorithmProblem
+{
+    class _1676_factorial_0_count
+    {
+        static void Problem_1676()
+        {
+            StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+            StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+
+            int n = int.Parse(sr.ReadLine());
+            int nCount = 0;
+            for(int i = 5; i <= n; i*=5)
+            {
+                nCount += n / i;
+            }
+            sw.WriteLine(nCount);
+            sw.Flush();
+            sr.Close();
+            sw.Close();
+
+            return;
+        }
+    }
+}
