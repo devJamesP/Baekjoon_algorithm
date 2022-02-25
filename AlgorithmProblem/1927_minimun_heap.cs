589e7fd329fbebb36fde96c446e2857526e250e4
James <znzldh1234@gmail.com>
2022-02-25 22:45:32 +0900

    1927 solution

---
+++ AlgorithmProblem/1927_minimun_heap.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace AlgorithmProblem
+{
+    class Heap
+    {
+        protected int[] nArr; // 배열
+        private int capacity; // 배열 크기
+        public int Capacity { get { return capacity; } }
+        protected int length; // 값이 들어있는 배열 크기
+
+        public Heap(int capacity)
+        {
+            nArr = new int[capacity];
+            this.capacity = capacity;
+            this.length = 0;
+        }
+        // empty 체크
+        public bool IsEmpty()
+        {
+            return length == 0;
+        }
+
+        // 최소값
+        public int Peek()
+        {
+            if (IsEmpty() == true)
+            {
+                return -1;
+            }
+            return nArr[0];
+        }
+
+        // 교체
+        protected void swap(int ndx1, int ndx2)
+        {
+            int temp = nArr[ndx1];
+            nArr[ndx1] = nArr[ndx2];
+            nArr[ndx2] = temp;
+        }
+
+        // 배열의 크기 유효성 체크
+        protected bool isInvalidate(int ndx)
+        {
+            return ndx >= capacity;
+        }
+
+        // 부모 인덱스
+        protected int getParentIndex(int ndx)
+        {
+            return (ndx - 1) / 2;
+        }
+
+        // 왼쪽 자식 인덱스
+        protected int getLeftChildIndex(int ndx)
+        {
+            int cdx = (ndx * 2) + 1;
+            return cdx;
+        }
+
+        // 오른쪽 자식 인덱스
+        protected int getRightChildIndex(int ndx)
+        {
+            int cdx = (ndx * 2) + 2;
+            return cdx;
+        }
+
+        // 리사이징
+        protected void ReSize()
+        {
+            int[] tempArr = nArr;
+            nArr = new int[capacity * 2];
+            for (int i = 0; i < tempArr.Length; ++i)
+            {
+                nArr[i] = tempArr[i];
+            }
+
+            capacity *= 2;
+
+            return;
+        }
+    }
+
+    class MinHeap : Heap
+    {
+        public MinHeap(int capacity) : base(capacity) { }
+
+        // 데이터 추가
+        public void Add(int data)
+        {
+            if (length == Capacity)
+            {
+                ReSize();
+            }
+
+            nArr[length] = data;
+            sortUp(length);
+            ++length;
+            return;
+        }
+
+        // 데이터 삭제
+        public int Delete()
+        {
+            if (IsEmpty() == true)
+            {
+                return 0;
+            }
+
+            int temp = nArr[0];
+            int ndx = sortDown();
+            nArr[length-1] = 0;
+
+            sortUp(ndx);
+
+            --length;
+            return temp;
+        }
+
+        // 추가 후 정렬
+        private void sortUp(int ndx)
+        {
+            if (nArr[ndx] == 0)
+            {
+                return;
+            }
+            int pdx = getParentIndex(ndx);
+            while (nArr[pdx] > nArr[ndx])
+            {
+                swap(pdx, ndx);
+                ndx = pdx;
+                pdx = getParentIndex(ndx);
+            }
+
+            return;
+        }
+
+        // 삭제 전 정렬
+        private int sortDown()
+        {
+            int ndx = 0;
+
+            int cdx1;
+            int cdx2;
+
+            int mdx;
+            while (true)
+            {
+                cdx1 = getLeftChildIndex(ndx);
+                cdx2 = getRightChildIndex(ndx);
+                if (isInvalidate(cdx1) == true || isInvalidate(cdx2) == true)
+                {
+                    break;
+                }
+                mdx = Min(cdx1, cdx2);
+                if (nArr[mdx] == 0)
+                {
+                    break;
+                }
+                swap(ndx, mdx);
+                ndx = mdx;
+            }
+
+            swap(ndx, length - 1);
+
+            return ndx;
+        }
+
+        private int Min(int ndx1, int ndx2)
+        {
+            if (nArr[ndx1] == 0)
+            {
+                return ndx2;
+            }
+            else if (nArr[ndx2] == 0)
+            {
+                return ndx1;
+            }
+            else
+            {
+                return nArr[ndx1] < nArr[ndx2] ? ndx1 : ndx2;
+            }
+        }
+    }
+
+    class _1927_minimun_heap
+    {
+        static void Problem_1927()
+        {
+            StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+            StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+
+            int N = int.Parse(sr.ReadLine());
+            MinHeap heap = new MinHeap(N);
+            for (int i = 0; i < N; ++i)
+            {
+                int n = int.Parse(sr.ReadLine());
+                if (n == 0)
+                {
+                    sw.WriteLine(heap.Delete());
+                }
+                else
+                {
+                    heap.Add(n);
+                }
+            }
+
+            sw.Flush();
+            sr.Close();
+            sw.Close();
+
+            return;
+        }
+    }
+}
---
+++ AlgorithmProblem/Heap_Implemente.cs
@@ -0,0 +1,214 @@
+﻿using System;
+
+namespace AlgorithmProblem
+{
+    class Heap
+    {
+        protected int[] arr;
+        protected int capacity;
+        protected int length;
+
+        public int Length { get { return length; } }
+        public int Capacity { get { return capacity; } }
+        public int Peek { get { return arr[0]; } }
+        protected Heap(int capacity)
+        {
+            arr = new int[capacity];
+            this.capacity = capacity;
+            this.length = 0;
+        }
+
+        protected void Swap(int ndx1, int ndx2)
+        {
+            int temp = arr[ndx1];
+            arr[ndx1] = arr[ndx2];
+            arr[ndx2] = temp;
+        }
+
+        // 부모 인덱스
+        protected int GetParentIndex(int cur)
+        {
+            return (cur - 1) / 2;
+        }
+
+        // 왼쪽 자식 인덱스
+        protected int GetLeftChildIndex(int cur)
+        {
+            int ndx = cur * 2 + 1;
+            return ndx;
+        }
+
+        // 오른쪽 자식 인덱스
+        public int GetRightChildIndex(int cur)
+        {
+            int ndx = cur * 2 + 2;
+            return ndx;
+        }
+
+        // 부모 반환 메소드
+        protected int GetParent(int cur)
+        {
+            int ndx = GetParentIndex(cur);
+            return arr[ndx];
+        }
+
+        // 왼쪽 자식 메소드
+        protected int GetLeftChild(int cur)
+        {
+            int ndx = GetLeftChildIndex(cur);
+            if (IsInvalidate(ndx) == true)
+            {
+                return -1;
+            }
+            return arr[ndx];
+        }
+
+        // 오른쪽 자식 메소드
+        protected int GetRightChild(int cur)
+        {
+            int ndx = GetRightChildIndex(cur);
+            if (IsInvalidate(ndx) == true)
+            {
+                return -1;
+            }
+            return arr[ndx];
+        }
+
+        // 유효성 체크
+        protected bool IsInvalidate(int cur)
+        {
+            return cur >= capacity;
+        }
+
+        // 리사이징
+        protected void ReSize()
+        {
+            int[] tempArr = arr;
+            arr = new int[capacity * 2];
+
+            for(int i = 0; i < capacity; ++i)
+            {
+                arr[i] = tempArr[i];
+            }
+            capacity <<= 1;
+        }
+
+        protected int Min(int n1, int n2) 
+        {
+            int min = 0;
+            if (arr[n1] == 0)
+            {
+                min = n2;
+            }
+            else if (arr[n2] == 0)
+            {
+                min = n1;
+            }
+            else
+            {
+                min = arr[n1] < arr[n2] ? n1 : n2;
+            }
+            return min;
+        }
+    }
+
+    class MinHeap : Heap
+    {
+        // 최소힙에 데이터 추가
+        public void Add(int data)
+        {
+            if (length == capacity)
+            {
+                ReSize();
+            }
+            int ndx = length;
+            for(int i = 0; i < arr.Length; ++i)
+            {
+                if (arr[i] == 0)
+                {
+                    arr[i] = data;
+                    ndx = i;
+                    break;
+                }
+            }
+            bubbleUp(ndx);
+            ++length;
+        }
+        void bubbleUp(int ndx)
+        {
+            int child = ndx;
+            int parent = GetParentIndex(child);
+            while (arr[parent] > arr[child])
+            {
+                Swap(parent, child);
+                parent = GetParentIndex(parent);
+            }
+
+            return;
+        }
+
+        // 최소힙의 최소값 제거
+        public int Delete()
+        {
+            int delValue = arr[0];
+            bubbleDown();
+            --length;
+            return delValue;
+        }
+
+        void bubbleDown()
+        {
+            int ndx = 0;
+            int child1;
+            int child2;
+            while(true)
+            {
+                child1 = GetLeftChildIndex(ndx);
+                child2 = GetRightChildIndex(ndx);
+                if (IsInvalidate(child1) == true || IsInvalidate(child2) == true)
+                {
+                    break;
+                }
+                int min = Min(child1, child2);
+                Swap(ndx, min);
+                ndx = min;
+            }
+            arr[ndx] = 0;
+            return;
+        }
+
+        public MinHeap(int length) : base(length) { }
+    }
+
+
+    class Heap_Implemente
+    {
+        static void Main(string[] args)
+        {
+            MinHeap minHeap = new MinHeap(7);
+            for(int i = 0; i < minHeap.Capacity; ++i)
+            {
+                minHeap.Add(i + 1);
+            }
+
+            Console.WriteLine(minHeap.Peek);
+            minHeap.Delete();
+            Console.WriteLine(minHeap.Peek);
+            minHeap.Delete();
+            Console.WriteLine(minHeap.Peek);
+            minHeap.Delete();
+            Console.WriteLine(minHeap.Peek);
+            minHeap.Delete();
+
+            Console.WriteLine(minHeap.Peek);
+            minHeap.Add(4);
+            Console.WriteLine(minHeap.Peek);
+            minHeap.Add(6);
+            Console.WriteLine(minHeap.Peek);
+            minHeap.Add(9);
+            Console.WriteLine(minHeap.Peek);
+            minHeap.Add(2);
+
+        }
+    }
+}
