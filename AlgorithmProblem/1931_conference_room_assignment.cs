5ba71190125077746121e2eb8738a1b1b94e58fe
James <znzldh1234@gmail.com>
2022-02-28 20:27:16 +0900

    1931 solution

---
+++ AlgorithmProblem/1931_conference_room_assignment.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.IO;
+
+namespace AlgorithmProblem
+{
+    class CTime : IComparable<CTime>
+    {
+        int nIn;
+        public int InTime { get { return nIn; } }
+
+        int nExit;
+        public int ExitTime { get { return nExit; } }
+        public CTime(int nIn, int nExit)
+        {
+            this.nIn = nIn;
+            this.nExit = nExit;
+        }
+
+        public int CompareTo(CTime other)
+        {
+            if (ExitTime < other.ExitTime)
+            {
+                return -1;
+            }
+            else if (ExitTime == other.ExitTime)
+            {
+                if (InTime < other.InTime)
+                {
+                    return -1;
+                }
+                else if (InTime > other.InTime)
+                {
+                    return 1;
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+            else
+            {
+                return 1;
+            }
+        }
+    }
+
+    class _1931_conference_room_assignment
+    {
+        static void Problem_1931()
+        {
+            StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+            StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+
+            int N = int.Parse(sr.ReadLine());
+            CTime[] aTime = new CTime[N];
+            int nCnt = 1;
+
+            // input
+            for (int i = 0; i < N; ++i)
+            {
+                string[] aInput = sr.ReadLine().Split();
+                int nIn = int.Parse(aInput[0]);
+                int nExit = int.Parse(aInput[1]);
+                aTime[i] = new CTime(nIn, nExit);
+            }
+
+            // sort
+            Array.Sort(aTime);
+
+            // calc
+            int nTime = aTime[0].ExitTime;
+            for (int i = 1; i < N; ++i)
+            {
+                if (nTime <= aTime[i].InTime)
+                {
+                    nTime = aTime[i].ExitTime;
+                    ++nCnt;
+                }
+            }
+
+            // output
+            sw.WriteLine(nCnt);
+
+            sw.Flush();
+            sr.Close();
+            sw.Close();
+
+            return;
+        }
+    }
+}
--- AlgorithmProblem/Heap_Implemente.cs
+++ AlgorithmProblem/Heap_Implemente.cs
@@ -1,214 +1,94 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace AlgorithmProblem
 {
-    class Heap
-    {
-        protected int[] arr;
-        protected int capacity;
-        protected int length;
-
-        public int Length { get { return length; } }
-        public int Capacity { get { return capacity; } }
-        public int Peek { get { return arr[0]; } }
-        protected Heap(int capacity)
-        {
-            arr = new int[capacity];
-            this.capacity = capacity;
-            this.length = 0;
-        }
-
-        protected void Swap(int ndx1, int ndx2)
-        {
-            int temp = arr[ndx1];
-            arr[ndx1] = arr[ndx2];
-            arr[ndx2] = temp;
-        }
-
-        // 부모 인덱스
-        protected int GetParentIndex(int cur)
-        {
-            return (cur - 1) / 2;
-        }
-
-        // 왼쪽 자식 인덱스
-        protected int GetLeftChildIndex(int cur)
-        {
-            int ndx = cur * 2 + 1;
-            return ndx;
-        }
-
-        // 오른쪽 자식 인덱스
-        public int GetRightChildIndex(int cur)
-        {
-            int ndx = cur * 2 + 2;
-            return ndx;
-        }
-
-        // 부모 반환 메소드
-        protected int GetParent(int cur)
-        {
-            int ndx = GetParentIndex(cur);
-            return arr[ndx];
-        }
-
-        // 왼쪽 자식 메소드
-        protected int GetLeftChild(int cur)
-        {
-            int ndx = GetLeftChildIndex(cur);
-            if (IsInvalidate(ndx) == true)
-            {
-                return -1;
-            }
-            return arr[ndx];
-        }
-
-        // 오른쪽 자식 메소드
-        protected int GetRightChild(int cur)
-        {
-            int ndx = GetRightChildIndex(cur);
-            if (IsInvalidate(ndx) == true)
-            {
-                return -1;
-            }
-            return arr[ndx];
-        }
-
-        // 유효성 체크
-        protected bool IsInvalidate(int cur)
-        {
-            return cur >= capacity;
-        }
-
-        // 리사이징
-        protected void ReSize()
-        {
-            int[] tempArr = arr;
-            arr = new int[capacity * 2];
-
-            for(int i = 0; i < capacity; ++i)
-            {
-                arr[i] = tempArr[i];
-            }
-            capacity <<= 1;
-        }
-
-        protected int Min(int n1, int n2) 
-        {
-            int min = 0;
-            if (arr[n1] == 0)
-            {
-                min = n2;
-            }
-            else if (arr[n2] == 0)
-            {
-                min = n1;
-            }
-            else
-            {
-                min = arr[n1] < arr[n2] ? n1 : n2;
-            }
-            return min;
-        }
-    }
-
-    class MinHeap : Heap
-    {
-        // 최소힙에 데이터 추가
-        public void Add(int data)
-        {
-            if (length == capacity)
-            {
-                ReSize();
-            }
-            int ndx = length;
-            for(int i = 0; i < arr.Length; ++i)
-            {
-                if (arr[i] == 0)
-                {
-                    arr[i] = data;
-                    ndx = i;
-                    break;
-                }
-            }
-            bubbleUp(ndx);
-            ++length;
-        }
-        void bubbleUp(int ndx)
-        {
-            int child = ndx;
-            int parent = GetParentIndex(child);
-            while (arr[parent] > arr[child])
-            {
-                Swap(parent, child);
-                parent = GetParentIndex(parent);
-            }
-
-            return;
-        }
-
-        // 최소힙의 최소값 제거
-        public int Delete()
-        {
-            int delValue = arr[0];
-            bubbleDown();
-            --length;
-            return delValue;
-        }
-
-        void bubbleDown()
-        {
-            int ndx = 0;
-            int child1;
-            int child2;
-            while(true)
-            {
-                child1 = GetLeftChildIndex(ndx);
-                child2 = GetRightChildIndex(ndx);
-                if (IsInvalidate(child1) == true || IsInvalidate(child2) == true)
-                {
-                    break;
-                }
-                int min = Min(child1, child2);
-                Swap(ndx, min);
-                ndx = min;
-            }
-            arr[ndx] = 0;
-            return;
-        }
-
-        public MinHeap(int length) : base(length) { }
-    }
-
-
     class Heap_Implemente
     {
-        static void Main(string[] args)
+        static void Main2(string[] args)
         {
-            MinHeap minHeap = new MinHeap(7);
-            for(int i = 0; i < minHeap.Capacity; ++i)
+            int[,] wires = new int[3, 2] { { 1, 2 }, { 2, 3 }, { 3, 4 } };
+            int n = 4;
+            solution(n, wires);
+        }
+
+        static public int solution(int n, int[,] wires)
+        {
+            bool[] visited = new bool[n + 1];
+            bool[,] graph = new bool[n + 1, n + 1];
+            int[] posArr = new int[2];
+            int answer = -1;
+            int min = n;
+
+            // setup
+            setupGraph(graph, wires);
+
+            // min count
+            for (int i = 0; i < wires.GetLength(0); ++i)
             {
-                minHeap.Add(i + 1);
+                posArr[0] = wires[i, 0];
+                posArr[1] = wires[i, 1];
+                int cnt = count(n, posArr, visited, graph);
+                min = min < cnt ? min : cnt;
             }
 
-            Console.WriteLine(minHeap.Peek);
-            minHeap.Delete();
-            Console.WriteLine(minHeap.Peek);
-            minHeap.Delete();
-            Console.WriteLine(minHeap.Peek);
-            minHeap.Delete();
-            Console.WriteLine(minHeap.Peek);
-            minHeap.Delete();
+            // answer
+            answer = min;
+            return answer;
+        }
 
-            Console.WriteLine(minHeap.Peek);
-            minHeap.Add(4);
-            Console.WriteLine(minHeap.Peek);
-            minHeap.Add(6);
-            Console.WriteLine(minHeap.Peek);
-            minHeap.Add(9);
-            Console.WriteLine(minHeap.Peek);
-            minHeap.Add(2);
+        static void setupGraph(bool[,] graph, int[,] wires)
+        {
+            for (int i = 0; i < wires.GetLength(0); ++i)
+            {
+                int fromPos = wires[i, 0];
+                int toPos = wires[i, 1];
+                graph[fromPos, toPos] = true;
+                graph[toPos, fromPos] = true;
+            }
 
+            return;
+        }
+
+        static int count(int n, int[] posArr, bool[] visited, bool[,] graph)
+        {
+            Stack<int> stack = new Stack<int>();
+            int[] cntArr = new int[2];
+            int v;
+
+            for (int i = 0; i < cntArr.Length; ++i)
+            {
+                visited[posArr[i]] = true;
+                stack.Push(posArr[i]);
+                ++cntArr[i];
+
+                while (stack.Count > 0)
+                {
+                    v = stack.Pop();
+                    for (int j = 1; j < visited.Length; ++j)
+                    {
+                        if (visited[j] == false && graph[v, j] == true)
+                        {
+                            stack.Push(v);
+                            stack.Push(j);
+                            ++cntArr[i];
+                            break;
+                        }
+                    }
+                }
+                clear(visited);
+            }
+            return cntArr[0] < cntArr[1] ? cntArr[0] : cntArr[1];
+        }
+
+        static void clear(bool[] visited)
+        {
+            for (int i = 1; i < visited.Length; ++i)
+            {
+                visited[i] = false;
+            }
+            return;
         }
     }
+    
 }
