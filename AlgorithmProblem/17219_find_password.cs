468ae88db6899734620cee6c6ee0cac56dae56de
James <znzldh1234@gmail.com>
2022-03-06 14:04:05 +0900

    17219 solution

---
+++ AlgorithmProblem/17219_find_password.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace AlgorithmProblem
+{
+    class _17219_find_password
+    {
+        static void Problem_17219()
+        {
+            StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+            StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+
+            Dictionary<string, string> notepad = new Dictionary<string, string>();
+            string[] NM = sr.ReadLine().Split(' ');
+            int N = int.Parse(NM[0]);
+            int M = int.Parse(NM[1]);
+
+            // input
+            for(int i = 0; i < N; ++i)
+            {
+                string[] SP = sr.ReadLine().Split(' ');
+                notepad.Add(SP[0], SP[1]);
+            }
+
+            // output
+            for(int j = 0; j < M; ++j)
+            {
+                sw.WriteLine(notepad[sr.ReadLine()]);
+            }
+
+            sw.Flush();
+            sr.Close();
+            sw.Close();
+
+            return;
+        }
+    }
+}
